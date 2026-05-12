156ecee59e7dba51c791ef96dc5fa19613b31566
@akakeh560 <akakeh560@gmail.com>
2026-05-12 15:00:35 +0300

    fix blur

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,53 @@
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react'
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-import { navLinks } from '../../constants/index.js'
+import { navLinks } from "../../constants/index.js";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 const Navbar = () => {
- useGSAP(() => {
-	const navTween = gsap.timeline({
-	 scrollTrigger: {
-		trigger: 'nav',
-		start: 'bottom top'
-	 }
-	});
-	
-	navTween.fromTo('nav', { backgroundColor: 'transparent' }, {
-	 backgroundColor: '#00000050',
-	 backgroundFilter: 'blur(10px)',
-	 duration: 1,
-	 ease: 'power1.inOut'
-	});
- })
- 
- return (
-	<nav>
-	 <div>
-		<a href="#home" className="flex items-center gap-2">
-		 <img src="/images/logo.png" alt="logo" />
-		 <p>Velvet Pour</p>
-		</a>
-		
-		<ul>
-		 {navLinks.map((link) => (
-			<li key={link.id}>
-			 <a href={`#${link.id}`}>{link.title}</a>
-			</li>
-		 ))}
-		</ul>
-	 </div>
-	</nav>
- )
-}
-export default Navbar
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    });
+
+    navTween.fromTo(
+      "nav",
+      {
+        backgroundColor: "transparent",
+        backdropFilter: "blur(0px)",
+      },
+      {
+        backgroundColor: "rgba(0, 0, 0, 0.3)",
+        backdropFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+  });
+
+  return (
+    <nav>
+      <div>
+        <a href="#home" className="flex items-center gap-2">
+          <img src="/images/logo.png" alt="logo" />
+          <p>Velvet Pour</p>
+        </a>
+
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
